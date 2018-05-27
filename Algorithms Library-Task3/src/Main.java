ba92c8dc3f2a5dfaf5f4744f31058e2d1d0fd0e6
root <root@gasimov>
2018-05-27 15:16:09 +0200

    KMP search

--- Algorithms Library-Task3/src/Main.java
+++ Algorithms Library-Task3/src/Main.java
@@ -28,7 +28,6 @@ public class Main {
 		
 		ZAlgorithm Z = new ZAlgorithm("salamsagolsalamasdasdsalamsalam", "salam");
 		
-		
 	}
 
 }
--- Algorithms Library-Task3/src/strings/KMP.java
+++ Algorithms Library-Task3/src/strings/KMP.java
@@ -1,16 +1,17 @@
 package strings;
 
+import java.util.ArrayList;
+
 public class KMP {
-	int []p;
-	public KMP(String s) {
-		p = new int[s.length()];
+	public  static int[]compute(String s) {
+		int[] p = new int[s.length()];
 		p[0] = 0;
 		int i = 1;
 		int M = s.length();
 		// length of the previous longest prefix suffix
 		int len = 0;
 		while(i < M) {
-			if(p[i] == p[len]) {
+			if(s.charAt(i) == s.charAt(len)) {
 				len++;
 				p[i] = len;
 				i++;
@@ -22,6 +23,28 @@ public class KMP {
 				}
 			}
 		}
+		return p;
+	}
+	public static ArrayList<Integer> search(String text, String pattern) {
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		int[] p = compute(pattern);
+		int i = 0;
+		int j = 0;
+		while(i < pattern.length()) {
+			if(pattern.charAt(j) == text.charAt(i)) {
+				i++;
+				j++;
+			}
+			if(j == pattern.length()) {
+				ans.add(i - j);
+				j = p[j-1];
+			}else if(i < text.length() && pattern.charAt(j) != text.charAt(i)) {
+				if(j != 0) {
+					j = p[j-1];
+				}else i = i + 1;
+			}
+		}
+		return ans;
 	}
 
 }
