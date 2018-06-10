8f9f7dd01f25047723decbf71cd7e00e2dc366de
root <root@gasimov>
2018-06-10 23:43:47 +0200

    GCD extend

---
+++ Algorithms Library-Task3/src/numbertheory/GCD.java
@@ -0,0 +1,20 @@
+package numbertheory;
+
+public class GCD {
+	
+	public static long gcdextend(long a, long b, long[] X) {
+        if (b == 0) {
+            X[0] = 1;
+            X[1] = 0;
+            return a;
+        } else {
+            long d = gcdextend(b, a % b, X);
+            long x = X[1];
+            long y = X[0] - X[1] * (a / b);
+            X[0] = x;
+            X[1] = y;
+            return d;
+        }
+    }
+
+}
