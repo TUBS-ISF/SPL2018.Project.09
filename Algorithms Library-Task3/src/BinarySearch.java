b34149f55baded7b8fe610c688ef53a9e0266a39
root <root@gasimov>
2018-06-10 21:17:04 +0200

    Trenary Search with abstract compare

--- Algorithms Library-Task3/src/BinarySearch.java
+++ Algorithms Library-Task3/src/BinarySearch.java
@@ -1,10 +1,10 @@
 public abstract class BinarySearch <T>{
-	public abstract boolean compare(T a, T b);
+	public abstract int compare(T a, T b);
     public int searchl(T[] a, int l, int r, T x) {
         int ans = -1;
         while(l <= r) {
             int m = (l + r) / 2; // 1 2 3 5 5 5 5 7
-            if(compare(x, a[m])) {
+            if(compare(x, a[m]) == 1) {
                 ans = m;
                 r = m - 1;
             }else {
@@ -17,7 +17,7 @@ public abstract class BinarySearch <T>{
         int ans = -1;
         while(l <= r) {
             int m = (l + r) / 2; // 1 2 3 5 5 5 5 7
-            if(compare(a[m], x)) {
+            if(compare(a[m], x) == 1) {
                 ans = m;
                 l = m + 1;
             }else {
