3f708873671e62c0fb7d54ef9e8dcd0aa03da470
root <root@gasimov>
2018-06-10 23:24:53 +0200

    Instanz von Fenwick

--- Algorithms Library-Task3/src/Main.java
+++ Algorithms Library-Task3/src/Main.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import numbertheory.BigInt;
 import numbertheory.Matrix;
+import queryupdate.FenwickTree;
 import queryupdate.SegmentTree;
 import queryupdate.SparseTable;
 import strings.KMP;
@@ -18,6 +19,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		//#define KMP
+		Integer a = 12;
+		FenwickTree<Integer> T = new FenwickTree<Integer>(Integer.class, 100) {
+			@Override
+			public Integer add(Integer a, Integer b) {
+				// TODO Auto-generated method stub
+				return a  + b;
+			}
+		};
 	}
 
 }
