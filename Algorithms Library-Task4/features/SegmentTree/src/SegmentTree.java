a236585dee00a66255c6b1787c69b0879381ad24
root <root@gasimov>
2018-06-17 00:16:51 +0200

    Test

---
+++ Algorithms Library-Task4/features/SegmentTree/src/SegmentTree.java
@@ -0,0 +1,10 @@
+import interfaces.IMessage;
+
+public class SegmentTree implements IMessage{
+
+	@Override
+	public String getMessage() {
+		return "Salam";
+	}
+
+}
---
+++ Algorithms Library-Task4/src/interfaces/IMessage.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface IMessage {
+	public String getMessage();
+}
