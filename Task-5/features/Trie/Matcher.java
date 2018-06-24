ca639acaae1e8ae812be1d169b796d916cd5a800
Toghrul Gasimov <t.gasimov@tu-bs.de>
2018-06-25 01:32:42 +0200

    MAtcher with Trie https://en.wikipedia.org/wiki/Trie

---
+++ Task-5/features/Trie/Matcher.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+
+
+
+
+public class Matcher {
+	public  class Trie {
+		public  class Node {
+			public char c;
+			public int d;
+			public Map<Character, Node> M;
+			public Node(char c) {
+				this.c = c;
+				this.d = 1;
+				M = new HashMap<Character, Node>();
+			}
+		}
+		Node root;
+		public Trie() {
+			root = new Node(' ');
+		}
+		public void insert(String s) {
+			Node t = root;
+			for(int i = 0; i < s.length(); i++) {
+				Node k = t.M.get(s.charAt(i));
+				if(k == null) {
+					k = new Node(s.charAt(i));
+					t.M.put(s.charAt(i), k);
+					t = k;
+				}else {
+					
+					t = k;
+					t.d++;
+				}
+			}
+		}
+		public void delete(String s) {
+			Node t = root;
+			for(int i = 0; i < s.length(); i++) {
+				Node k = t.M.get(s.charAt(i));
+				if(k == null) {
+					return;
+				}else {
+					t = k;
+					t.d--;
+				}
+			}
+		}
+		public int search(String s) {
+			Node t = root;
+			for(int i = 0; i < s.length(); i++) {
+				t = t.M.get(s.charAt(i));
+				if(t == null) return 0;
+			}
+			return t.d;
+		}
+	}
+	public ArrayList<Integer> search(String p, String text) {
+		// not optimal
+		Trie t = new Trie();
+		int n = text.length();
+		for(int i = 0; i < n; i++) {
+			t.insert(text.substring(i));
+		}
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		ans.add(t.search(p));
+		return ans;
+	}
+}
